628937418a6b52a62ad4f244031d40f15b1481b2
Luigi Belcastro <luigi.belcastro@liu.se>
2019-07-26 16:27:24 +0200

    Added gamma calibration files and utilities folder

---
+++ common/utilities/calibrateGamma.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 26 16:25:41 2019
+
+@author: Luigi Belcastro - Linköping University
+email: luigi.belcastro@liu.se
+
+Script to calibrate for the gamma correction of projector
+"""
