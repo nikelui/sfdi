8159410941e377947e1433b58c63d7632e0fc85f
Luigi <luigi.belcastro@liu.se>
2020-01-09 14:07:16 +0100

    Created new subfolder for Qt5 GUI

---
+++ gui/test.py
@@ -0,0 +1,11 @@
+import sys
+from PyQt5 import QtWidgets
+
+def main():
+    sizeObject = QtWidgets.QDesktopWidget().screenGeometry(1)
+    print(str(sizeObject.height()) +' x '+ str(sizeObject.width()))
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    main()
+    sys.exit(app.exec_())
