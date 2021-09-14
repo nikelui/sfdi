bf4c076c4064ba4c6f69473424789e77ba81a623
Luigi Belcastro <luigi.belcastro@liu.se>
2021-09-14 15:55:31 +0200

    Saving one image at a time (memory costrains)

--- common/sfdi/acquisitionRoutine_gui.py
+++ common/sfdi/acquisitionRoutine_gui.py
@@ -12,14 +12,22 @@ import time, os
 from sfdi.sinPattern import sinPattern
 import threading
 
-def saveFunc(nFreq, nPhase, nchannels, curr_path, name, dataMat):
+# def saveFunc(nFreq, nPhase, nchannels, curr_path, name, dataMat):
+#     """Function to save data, run in a separate thread"""
+#     for i in range(nchannels*3):
+#         for j in range(nFreq+1):
+#             for k in range(nPhase):
+#                 im = PIL.fromarray(dataMat[:,:,k + (nPhase*j) + i*(nPhase*(nFreq+1))].astype('uint8'))
+#                 im.save('{}/{}_{:d}-{:d}-{:d}.bmp'.format(curr_path, name, i, j, k))
+#     print("Pictures saved to: {}\n".format(curr_path))
+
+def saveFunc(i, j, k, nchannels, curr_path, name, frame):
     """Function to save data, run in a separate thread"""
-    for i in range(nchannels*3):
-        for j in range(nFreq+1):
-            for k in range(nPhase):
-                im = PIL.fromarray(dataMat[:,:,k + (nPhase*j) + i*(nPhase*(nFreq+1))].astype('uint8'))
-                im.save('{}/{}_{:d}-{:d}-{:d}.bmp'.format(curr_path, name, i, j, k))
-    print("Pictures saved to: {}\n".format(curr_path))
+    for _i in range(nchannels):
+        im = PIL.fromarray(frame[:,:,_i].astype('uint8'))
+        im.save('{}/{}_{:d}-{:d}-{:d}.bmp'.format(curr_path, name, i*nchannels+_i, j, k),
+                format='BMP', compression='raw')
+        # print("Pictures saved to: {}\n".format(curr_path))
     
 def acquisitionRoutine(root):
     """Routine to acquire a full SFDI image set and save data, now with threading.
@@ -63,9 +71,14 @@ NOTE: to work correctly, you need to have an OpenCV window called 'pattern' show
     
     # Retrieve camera resolution
     H,W = root.cam.getResolution()  # height x width
-    dataMat = np.zeros((H, W,nPhase*(nFreq+1)*nchannels*3),dtype=float)
-       
+    # dataMat = np.zeros((H, W,nPhase*(nFreq+1)*nchannels*3),dtype=float)
     root.stop = False # Insert a flag for stopping
+    
+    # get exposure time, to normalize RAW data
+    expt = int(root.cam.getExposure())
+    curr_path = '{}/{:d}_{:s}_{:d}ms'.format(outPath, t_stamp, fname, expt)  # create one folder for each timestamp
+    if not os.path.exists(curr_path):
+        os.makedirs(curr_path)
     
     ## New: increase the exposure time for blue channel (SNR is very low on tissues)
     ## consider to use a slightly longer pause between patterns
@@ -91,9 +104,9 @@ NOTE: to work correctly, you need to have an OpenCV window called 'pattern' show
             else:
                 time.sleep(dt/1000)
             frame = root.cam.capture(nframes=1, save=False)
-            # Change approach: keep everything in a big matrix and save later
-            for ch in range(nchannels):
-                dataMat[:,:, p + (nPhase*i) + ch*(nPhase*(nFreq+1))] = frame[:,:,ch]
+            ## New: save single images (because of memory constrains)
+            th = threading.Thread(target=saveFunc,args=(0, i, p, nchannels, curr_path, name, frame,))
+            th.start()
     
     if (root.par['blueBoost']):
         # Here, return exposure to previous value
@@ -111,9 +124,12 @@ NOTE: to work correctly, you need to have an OpenCV window called 'pattern' show
             root.PatternCanvas.create_image(0, 0, image=pattern, anchor=NW)
             time.sleep(dt/1000)
             frame = root.cam.capture(nframes=1, save=False)
+            ## New: save single images (because of memory constrains)
+            th = threading.Thread(target=saveFunc,args=(1, i, p, nchannels, curr_path, name, frame,))
+            th.start()
             # Change approach: keep everything in a big matrix and save later
-            for ch in range(nchannels):
-                dataMat[:,:, p + (nPhase*i) + (ch + nchannels)*(nPhase*(nFreq+1))] = frame[:,:,ch]
+            # for ch in range(nchannels):
+            #     dataMat[:,:, p + (nPhase*i) + (ch + nchannels)*(nPhase*(nFreq+1))] = frame[:,:,ch]
     ##time.sleep(dt/2000)
     
     # Acquire RED
@@ -128,21 +144,15 @@ NOTE: to work correctly, you need to have an OpenCV window called 'pattern' show
             root.PatternCanvas.create_image(0, 0, image=pattern, anchor=NW)
             time.sleep(dt/1000)
             frame = root.cam.capture(nframes=1, save=False)
+            ## New: save single images (because of memory constrains)
+            th = threading.Thread(target=saveFunc,args=(2, i, p, nchannels, curr_path, name, frame,))
+            th.start()
             # Change approach: keep everything in a big matrix and save later
-            for ch in range(nchannels):
-                dataMat[:,:, p + (nPhase*i) + (ch + nchannels*2)*(nPhase*(nFreq+1))] = frame[:,:,ch]
-                
+            # for ch in range(nchannels):
+            #     dataMat[:,:, p + (nPhase*i) + (ch + nchannels*2)*(nPhase*(nFreq+1))] = frame[:,:,ch]
     end = time.time()   
-    print("Capture time: {:.2f} seconds\n".format(float(end-start)))
+    print("Capture time: {:.2f} seconds\n".format(float(end-start)))   
     
-    # get exposure time, to normalize RAW data
-    expt = root.cam.getExposure()
-    
-    # for loop to save on file
-    curr_path = (outPath + '/%d_%s_%dms' % (t_stamp,fname,expt)) # create one folder for each timestamp
-    if not os.path.exists(curr_path):
-        os.makedirs(curr_path)
-    
-    th = threading.Thread(target=saveFunc,args=(nFreq,nPhase,nchannels,curr_path,name,dataMat,))
-    th.start()
+    # th = threading.Thread(target=saveFunc,args=(nFreq,nPhase,nchannels,curr_path,name,dataMat,))
+    # th.start()
     ## End
