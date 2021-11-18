4acd2e552c4c2525935bf99f0408a144be45f676
Luigi Belcastro <luigi.belcastro@liu.se>
2021-11-18 16:22:21 +0100

    New 2 layer model test

--- analysis/twoLayerModel.py
+++ analysis/twoLayerModel.py
@@ -10,6 +10,7 @@ import json
 import numpy as np
 from scipy.optimize import least_squares
 from sfdi.common.getPath import getPath
+from sfdi.common.readParams import readParams
 from sfdi.analysis.dataDict import dataDict
 
 def load_obj(name, path):
@@ -31,7 +32,7 @@ bm = measured b coefficient
     return ((x[0] * x[2]) + x[1]*(li - x[2])) - bm
 
 
-def new_two_layer_fun(x, delta, mus):
+def new_two_layer_fun(x, delta, mus, wv):
     """Partial volumes equation for a two layer model
 __________
 ____a_____ | |- (la, mus_a)
@@ -39,60 +40,67 @@ ____a_____ | |- (la, mus_a)
            |
 ____b____  |--- (delta, mus_b)
     
-    model: mus = (mus_a*la + mus_b*lb)/delta
+    model: mus = (a1*lambda^-b1 * la + 2*lambda^-b2 * (delta-la))/delta
 
     Parameters
     ----------
     x : FLOAT array
-        array of unknowns  ->  [mus_a, mus_b, la]
-        - mus_a -> scattering coefficient of layer a [mm^-1]
-        - mus_b -> scattering coefficient of layer b [mm^-1]
+        array of unknowns  ->  [a1, b1, a2, b2, la]
+        - a1, b1 -> scattering parameters of layer a [mm^-1]
+        - a2, b2 -> scattering parameters of layer b [mm^-1]
         - la -> thickness of layer a [mm]
     delta : FLOAT
         Estimated penetration depth of light [mm]
     mus : FLOAT
         Measured scattering coefficient [mm^-1]
+    wv : FLOAT
+        Wavelength (need for fitting)
 
     Returns
     -------
-    Difference between mus measurement and 2-layer model (to be used in least square)
+    Square difference between mus measurement and 2-layer model (to be used in least square)
 """
-    import pdb; pdb.set_trace()  # DEBUG
-    return (x[0]*x[2] + x[1]*(delta - x[2]))/delta - mus
+    return np.sum(((x[0]*wv[:,np.newaxis]**(-x[1]) * x[4]) + 
+                   (x[2]*wv[:,np.newaxis]**(-x[3]) * (delta[np.newaxis,:] - x[4])) / 
+                       delta - mus)**2, axis=1)
 
 
-def read_param(fpath):
-    params = {}
-    with open(fpath, 'r') as file:
-        for line in file.readlines():
-            if line.startswith('#') or len(line.strip()) == 0:  # ignore comments and newlines
-                pass
-            else:
-                key, item = (x.strip() for x in line.split('='))
-                if item.startswith('['):
-                    end = item.find(']')
-                    item = json.loads(item[:end+1])
-                params[key] = item
-    return params
+# def read_param(fpath):
+#     params = {}'
+#     with open(fpath, 'r') as file:
+#         for line in file.readlines():
+#             if line.startswith('#') or len(line.strip()) == 0:  # ignore comments and newlines
+#                 pass
+#             else:
+#                 key, item = (x.strip() for x in line.split('='))
+#                 if item.startswith('['):
+#                     end = item.find(']')
+#                     item = json.loads(item[:end+1])
+#                 params[key] = item
+#     return params
 
 #%% Load pre-processed data
 data_path = getPath('select data path')
-# par = read_param('{}/README.txt'.format(data_path))  # optional
+par = readParams('{}/processing_parameters.ini'.format(data_path))  # optional
 data = load_obj('dataset', data_path)
-# data.par = par
-ret = data.singleROI('SC2', fit='single', I=2e3, norm=-1)
+data.par = par
 
 #%% Least square fit
+ret = data.singleROI('AlO3ml', fit='single', I=3e3, norm=None)
+
 d = np.mean(ret['depths'], axis=1)  # delta/2
 d2 = np.mean(ret['depth_phi'], axis=1)  # 1/e * phi^2
 d3 = np.mean(ret['depth_MC'], axis=1)  # calculated via Monte Carlo model
 mus = ret['op_ave'][:,:,1]  # average measured scattering coefficient
 
-opt = least_squares(new_two_layer_fun, x0=[[1,1,1,1,1], [1,1,1,1,1], [0.1,0.1,0.1,0.1,0.1,]],
-                    kwargs={'delta': d, 'mus': mus}, bounds=[0, np.inf], method='trf')
-
-
-opt2 = least_squares(new_two_layer_fun, x0=[1, 1, 0.1], kwargs={'delta': d2[1:], 'mus': mus[1:]},
+opt = least_squares(new_two_layer_fun, x0=[100,1,100,1,0.1],
+                    kwargs={'delta': d, 'mus': mus.T, 'wv':np.array(data.par['wv'])},
                     bounds=[0, np.inf], method='trf')
-opt3 = least_squares(new_two_layer_fun, x0=[1, 1, 0.1], kwargs={'delta': d3[1:], 'mus': mus[1:]},
-                     bounds=[0, np.inf], method='trf')
+
+opt2 = least_squares(new_two_layer_fun, x0=[100,1,100,1,0.1],
+                    kwargs={'delta': d2, 'mus': mus.T, 'wv':np.array(data.par['wv'])},
+                    bounds=[0, np.inf], method='trf')
+
+opt3 = least_squares(new_two_layer_fun, x0=[100,1,100,1,0.1],
+                    kwargs={'delta': d3, 'mus': mus.T, 'wv':np.array(data.par['wv'])},
+                    bounds=[0, np.inf], method='trf')
