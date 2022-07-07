cf499c4d974a647fe7bbb77267bec5c96d639a13
Luigi Belcastro <luigi.belcastro@liu.se>
2022-07-07 11:28:07 +0900

    expanded README and docs

--- README.md
+++ README.md
@@ -1,4 +1,47 @@
 # sfdi
 A collection of Python scripts to acquire SFDI data and process it, in order to measure optical properties of tissue.
 
-**NOTE:** this project is intended for private use, it is not optimized to be distributed and used in other machines.
+**NOTE:** this project was developed for personal use, it is not optimized to be distributed
+and used in other machines. An extensive (and slow) re-work is currently in progress to make
+the library easier to install (e.g. frozen binaries) and to write better documentation.
+Any contribution is welcome.
+
+## Dependencies
+
+- Numpy
+- Scipy
+- Matplotlib
+- tkinter
+- OpenCv
+- [cvui](https://dovyski.github.io/cvui/#:~:text=cvui%20is%20a%20(very)%20simple,it%20OpenGL%20enabled%2C%20for%20instance.) (already included)
+- PyInstaller (to build frozen binaries)
+...
+
+## Acquisition
+**Coming soon**
+
+## Processing
+**Coming soon**
+
+## Cameras
+The SFDI module provides a camera class object that acts as a wrapper for the
+actual camera API code. For each camera you plan to use in the system, you need to
+install the respective drivers/SDK and implement the camera class methods
+(make a copy of DummyCam.py and read the descriptions / return values).
+### Currently implemented cameras
+- **DummyCam**: blank camera class with placeholders
+- **Generic**: OpenCV generic VideoCapture I/O module
+- **pointGrey**: old FLIR FlyCapture 
+[SDK](https://www.flir.com/products/flycapture-sdk/?vertical=machine%20vision&segment=iis)
+  - NOTE: this is obsolete software (last version is from 2019?). FLIR has released a new
+	SDK called [Spinnaker](https://www.flir.com/products/spinnaker-sdk/?vertical=machine%20vision&segment=iis),
+  which supports their new USB3 / GiGE cameras.
+- **piCam**: RaspbberryPi piCamera module (only works on linux arm system for RaspberryPi)
+- **IS**: Imaging Source camera [SDK](https://github.com/TheImagingSource)
+- **xiCam** [coming soon]: ximea camera [SDK](https://www.ximea.com/support/wiki/apis/Python)
+
+## Planned
+- Substitute OpenCV / cvui with native python libraries
+  - Use PIL for image processing / ROI selection
+  - Use TkInter for GUI elements
+- OpenCL for GPU parallel processing
--- camera/DummyCam.py
+++ camera/DummyCam.py
@@ -9,36 +9,55 @@ import numpy as np
 
 
 class DummyCam:
-    """A dummy camera object for debug purposes. It implements all the camera methods with placeholders
+    """A dummy camera object for debug purposes. It implements all the camera methods
+    with placeholders. Make a copy of this class for each camera and implement the
+    actual class methods.
+    
+    NOTE: in the __init__ section, you should set all the relevant camera initialization
+    (e.g. disable auto-exposure, gain and white balance, set the framerate/resolution...)
     """
     def __init__(self, num=0, res='640p', fps=60.0, **kwargs):
+        # Number of the camera color channels (1 for monochrome, 3 for RGB and N for multispectral)
         self.nchannels = 3
+        # Camera horizontal and vertical resolution (pixel)
         self.xRes = 640
         self.yRes = 480
         
     def capture(self):
-        # linear gradient, just to be more interesting
+        """This function should acquire one frame from the camera and return it as
+        a numpy array (uint8: values from 0-255)"""
+        # The dummy class returns a linear gradient, just to be more interesting
         A = np.linspace(0, 255, self.xRes)
         B = np.tile(A, [self.yRes, 1])
         C = np.stack((B, B, B), axis=2)
         return C.astype('uint8')
     
     def getResolution(self):
+        """Return the horizontal and vertical resolution of the camera (int: pixel)"""
         return (self.yRes, self.xRes)
     
     def getExposure(self):
+        """Return the current set exposure time (float: milliseconds).
+        NOTE: if the camera works with a different time unit, remember to convert."""
         return 1e2
     
     def getExposureLim(self):
+        """Return the min and max values for exposure and the increase/decrease
+        step (float: milliseconds)
+        NOTE: if the camera works with a different time unit, remember to convert."""
         return (1, 100, 0.1)
     
     def setExposure(self, exp):
+        """Set exposure time to exp (float: milliseconds)
+        NOTE: if the camera works with a different time unit, remember to convert."""
         pass
     
     def setFramerate(self, fps):
+        """Set the camera framerate (float: fps)"""
         pass
     
     def close(self):
+        """Stop acquisition, disconnect the camera and clear the object from memory."""
         pass
     
 if __name__ == '__main__':
