a0d0fc50380cdb90b58477839019b84a7e149bc0
Luigi Belcastro <luigi.belcastro@liu.se>
2022-10-25 09:59:16 +0200

    mus plots with errorbars (error in fx)

--- analysis/direct_model.py
+++ analysis/direct_model.py
@@ -46,9 +46,14 @@ if os.path.exists('{}/obj/dataset.pkl'.format(data_path)):
 dz = 0.01  # resolution
 thick = np.array([0.125, 0.265, 0.51, 0.67, 1.17])  # thickness of thin phantoms
 asd = loadmat(f'{data_path}/SFDS_8fx.mat')
+
+df = 0.05  # mm^-1
 fx = np.array([np.mean(par['fx'][i:i+4]) for i in range(len(par['fx'])-3)])
+fx_plus = fx + df
+fx_min = fx - df
+
 z = np.arange(0, 10, dz)
-lamb = 500  # nm
+lamb = 749  # nm
 WV = np.where(asd['wv'][:,0] >= lamb)[0][0]
 F = 0  # spatial frequency to plot
 
@@ -61,42 +66,71 @@ phi_diff = {}  # diffusion
 phi_diffusion = {}  # diffusion, Seo
 phi_deltaP1 = {}  # delta-P1, Vasen modified
 phi_dp1 = {}  # delta-P1, Seo original
+# Errorbars for delta-P1
+phi_deltaP1_plus = {}
+phi_deltaP1_min = {}
+phi_dp1_plus = {}
+phi_dp1_min = {}
 
 # models of alpha
 alpha_diff = {}
 alpha_diffusion = {}
 alpha_deltaP1 = {}
 alpha_dp1 = {}
+# Errorbars
+alpha_deltaP1_plus = {}
+alpha_deltaP1_min = {}
+alpha_dp1_min = {}
+alpha_dp1_plus = {}
+
 
 # Models of scattering
 mus_top = np.squeeze(asd['TiObaseTop'][:,:,1]).T
-# mus_top[:,:] = mus_top[0,:]  # To fix mus to a single value (f0)
+mus_top[:,:] = mus_top[0,:]  # To fix mus to a single value (f0)
 mus_bot = np.squeeze(asd['AlObaseTop'][:,:,1]).T
-# mus_bot[:,:] = mus_bot[0,:]  # To fix mus to a single value (f0)
+mus_bot[:,:] = mus_bot[0,:]  # To fix mus to a single value (f0)
 mus_meas = {k:asd[k][:,:,1].T for k in keys[1:-1]}
 
 mus_model_diff = {}
 mus_model_diffusion = {}
 mus_model_deltaP1 = {}
+mus_model_deltaP1_plus = {}
+mus_model_deltaP1_min = {}
 mus_model_dp1 = {}
+mus_model_dp1_plus = {}
+mus_model_dp1_min = {}
 
-power = 1  # to quickly change from phi to phi**2
+power = 2  # to quickly change from phi to phi**2
 for _i, key in enumerate(keys):
     phi_diff[key] = models.phi_diff(z, asd[key][:,:,0].T, asd[key][:,:,1].T/0.2, fx)  # diffusion
     phi_diffusion[key] = models.phi_diffusion(z, asd[key][:,:,0].T, asd[key][:,:,1].T/0.2, fx)  # diffusion, Seo
     phi_deltaP1[key] = models.phi_deltaP1(z, asd[key][:,:,0].T, asd[key][:,:,1].T/0.2, fx)  # d-p1, Luigi
+    phi_deltaP1_plus[key] = models.phi_deltaP1(z, asd[key][:,:,0].T, asd[key][:,:,1].T/0.2, fx_plus)
+    phi_deltaP1_min[key] = models.phi_deltaP1(z, asd[key][:,:,0].T, asd[key][:,:,1].T/0.2, fx_min)
     phi_dp1[key] = models.phi_dP1(z, asd[key][:,:,0].T, asd[key][:,:,1].T/0.2, fx)  # d-p1, Seo
+    phi_dp1_plus[key] = models.phi_dP1(z, asd[key][:,:,0].T, asd[key][:,:,1].T/0.2, fx_plus)
+    phi_dp1_min[key] = models.phi_dP1(z, asd[key][:,:,0].T, asd[key][:,:,1].T/0.2, fx_min)
     
-    if _i > 0 and _i < len(keys)-1 and False:
+    
+    
+    if _i > 0 and _i < len(keys)-1 and True:
         alpha_diff[key] = models.alpha(phi_diff[key]**power, z, thick[_i-1])
         alpha_diffusion[key] = models.alpha(phi_diffusion[key]**power, z, thick[_i-1])
         alpha_deltaP1[key] = models.alpha(phi_deltaP1[key]**power, z, thick[_i-1])
+        alpha_deltaP1_plus[key] = models.alpha(phi_deltaP1_plus[key]**power, z, thick[_i-1])
+        alpha_deltaP1_min[key] = models.alpha(phi_deltaP1_min[key]**power, z, thick[_i-1])
         alpha_dp1[key] = models.alpha(phi_dp1[key]**power, z, thick[_i-1])
+        alpha_dp1_plus[key] = models.alpha(phi_dp1_plus[key]**power, z, thick[_i-1])
+        alpha_dp1_min[key] = models.alpha(phi_dp1_min[key]**power, z, thick[_i-1])
         
         mus_model_diff[key] = alpha_diff[key] * mus_top + (1-alpha_diff[key])* mus_bot
         mus_model_diffusion[key] = alpha_diffusion[key] * mus_top + (1-alpha_diffusion[key])* mus_bot
         mus_model_deltaP1[key] = alpha_deltaP1[key] * mus_top + (1-alpha_deltaP1[key])* mus_bot
+        mus_model_deltaP1_plus[key] = alpha_deltaP1_plus[key] * mus_top + (1-alpha_deltaP1_plus[key])* mus_bot
+        mus_model_deltaP1_min[key] = alpha_deltaP1_min[key] * mus_top + (1-alpha_deltaP1_min[key])* mus_bot
         mus_model_dp1[key] = alpha_dp1[key] * mus_top + (1-alpha_dp1[key])* mus_bot
+        mus_model_dp1_plus[key] = alpha_dp1_plus[key] * mus_top + (1-alpha_dp1_plus[key])* mus_bot
+        mus_model_dp1_min[key] = alpha_dp1_min[key] * mus_top + (1-alpha_dp1_min[key])* mus_bot
 
 if False:  # piecewise continuous model
     phi2_diff = models.phi_2lp(thick, phi_diff['TiObaseTop'], phi_diff['AlObaseTop'], z)
@@ -202,7 +236,7 @@ if False:
 # Plot fluence, normalized
 if False:
     mua = np.ones((1,1), dtype=float) * 0.02
-    mus = np.ones((1,1), dtype=float) * 3.1 / (0.2)  # convert musp to mus
+    mus = np.ones((1,1), dtype=float) * 0.8 / (0.2)  # convert musp to mus
     
     fx = np.arange(0, 0.51, 0.05)
     fx = np.array([np.mean(fx[_i:_i+4]) for _i in range(len(fx)-3)])
@@ -221,7 +255,7 @@ if False:
     ax.plot(z, dp1.T / np.sum(dp1.T * dz, axis=0))
     ax.grid(True, linestyle=':')
     ax.set_xlim([0, 5])
-    ax.set_ylim([0, 1.75])
+    ax.set_ylim([0, 1])
     ax.set_xlabel('mm')
     ax.set_ylabel(r'$\varphi$', fontsize=14)
     ax.set_title(r"$\delta$-P1 - Seo ($\mu_a$ = {}, $\mu'_s$ = {})".format(mua[0][0], mus[0][0]*0.2))
@@ -233,7 +267,7 @@ if False:
     ax.plot(z, deltaP1.T / np.sum(deltaP1.T * dz, axis=0))
     ax.grid(True, linestyle=':')
     ax.set_xlim([0, 5])
-    ax.set_ylim([0, 1.75])
+    ax.set_ylim([0, 1])
     ax.set_xlabel('mm')
     ax.set_ylabel(r'$\varphi$', fontsize=14)
     ax.set_title(r"$\delta$-P1 - Luigi ($\mu_a$ = {}, $\mu'_s$ = {})".format(mua[0][0], mus[0][0]*0.2))
@@ -241,7 +275,7 @@ if False:
     fig.tight_layout()
 
 # Plot fluence(fx) normalized
-if True:
+if False:
     plt.rcParams["axes.prop_cycle"] = plt.cycler("color", cm.Purples(np.linspace(1, 0.2, len(fx))))
     for _i, key in enumerate(phi_deltaP1.keys()):
         fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(7,8), num=_i+1)
@@ -352,7 +386,38 @@ if False:
     fig.tight_layout()
     # ax.legend(framealpha=1, ncol=4)
     # ax.set_axis_off()
-        
+
+# Plot mus (only delta-P1) with top and bottom and errorbars
+if True:
+    # plt.rcParams["axes.prop_cycle"] = plt.cycler("color", cm.Reds(np.linspace(1, 0.2, len(fx))))
+    cmap = cm.get_cmap('Dark2')
+    # colors = [cmap(x) for x in np.linspace(0,1,len(mus_meas)+2)]
+    colors = ['lime', 'yellowgreen', 'sandybrown', 'skyblue', 'mediumorchid', 'gold' , 'orangered']
+    
+    # fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(7,4), num=1)
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(7,4.5), num=1)
+    ax.plot(fx, mus_top[:,WV], linestyle='solid', color=colors[0], label=r'TiO$_2$')
+    ax.plot(fx, mus_bot[:,WV], linestyle='solid', color=colors[-1], label=r'Al$_2$O$_3$')
+    for _i, key in enumerate(mus_meas.keys()):
+        # Luigi d-P1
+        ax.fill_between(fx, mus_model_deltaP1_plus[key][:,WV], mus_model_deltaP1_min[key][:,WV],
+                        alpha=0.5, edgecolor=colors[_i+1], facecolor=colors[_i+1])
+        ax.plot(fx, mus_model_deltaP1[key][:,WV], linestyle='dashed', color=colors[_i+1])
+        # Seo d-P1
+        # ax.fill_between(fx, mus_model_dp1_plus[key][:,WV], mus_model_dp1_min[key][:,WV],
+        #                 alpha=0.5, edgecolor=colors[_i+1], facecolor=colors[_i+1])
+        # ax.plot(fx, mus_model_dp1[key][:,WV], linestyle='dotted', color=colors[_i+1])
+        # Measured
+        ax.plot(fx, mus_meas[key][:,WV], 'o', color=colors[_i+1],
+                label=r'{}'.format(key))
+    ax.set_title(r'$\delta$-P1 - Luigi'+'\n'+r'$\Delta f_x$={}mm$^{{-1}}$'.format(df))
+    ax.grid(True, linestyle=':')
+    ax.set_xlabel(r'fx (mm$^{{-1}})$')
+    ax.set_ylabel(r"$\mu'_s$", fontsize=14)
+    fig.tight_layout()
+    # ax.legend(framealpha=1, ncol=4)
+    # ax.set_axis_off()
+
 # Plot relative errors
 if False:
     plt.rcParams["axes.prop_cycle"] = plt.cycler("color", cm.YlGn(np.linspace(1, 0.2, 5)))
