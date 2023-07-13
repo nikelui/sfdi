ca674d64afdb24d00056470517444c042514fa8e
Luigi Belcastro <luigi.belcastro@liu.se>
2023-07-13 17:52:09 +0200

    Finished Inverse solver script

--- analysis/InverseSolver.py
+++ analysis/InverseSolver.py
@@ -10,7 +10,7 @@ Script to get multi-fx scattering data on 2 layer tissue and estimate tissue thi
 layer specific scattering
 """
 from sfdi.common.getFile import getFile
-from sfdi.commons import models  # fluence models
+from sfdi.common import models  # fluence models
 SDA = models.phi_diff
 dP1 = models.phi_dP1
 mod_dP1 = models.phi_deltaP1
@@ -20,7 +20,7 @@ from scipy.io import loadmat, savemat
 from scipy.optimize import Bounds
 from scipy.optimize import least_squares
 
-def min_fun(x0, mua_meas, mus_meas, z, fx, model=SDA):
+def model_fun(x0, mua_meas, mus_meas, z, fx, model=SDA):
     """Function to calculate difference between the 2-layer model and the measurements
 Input:
     - x0 = [d, mus_top, mus_bot]
@@ -40,7 +40,13 @@ Input:
     a = alpha(phi, z, d)
     
     mus_model = a * mus_top + (1-a) * mus_bot
-    diff = np.sqrt((np.sum((mus_model - mus_meas)**2, axis=0) / mus_meas.shape[0]))
+    return mus_model
+
+def target_fun(x0, mua_meas, mus_meas, z, fx, model=SDA):
+    """Calculate residuals, for minimization using least squares"""    
+    mus_model = model_fun(x0, mua_meas, mus_meas, z, fx, model)
+    # diff = np.sqrt((np.sum((mus_model - mus_meas)**2, axis=0) / mus_meas.shape[0]))
+    diff = np.squeeze(mus_model - mus_meas)
     return diff
     
 def alpha(phi, z, d):
@@ -61,26 +67,56 @@ Input:
     - z [1 x Z]: depth axis
     - d [float]: thickness of top layer
 """
-    dz = z[1] - z[0]  # discrete depth step
+    dz = z[0,1] - z[0,0]  # discrete depth step
     # Find index to "integrate" up to d
-    idx = np.where(z >= d)[0][0]
+    idx = np.where(z >= d)[1][0]
     alpha = np.sum(phi[:,:,:idx]**2 * dz, axis=-1)/np.sum(phi**2 * dz, axis=-1)
     return alpha
 
+def jacob(x0, mua_meas, mus_meas, z, fx, model=SDA):
+    """Analytically compute jacobian matrix to be used in least squares
+NOTE: it needs the same input as the target function
+Input:
+    - x0 = [d, mus_t, mus_b]  (1 x 3 float)
+    - phi (N x M x Z float)
+    - z (1 x Z float)
+"""
+    d, mus_t, mus_b = x0  # unpack
+    dz = z[0,1] - z[0,0]  # discrete depth step
+    # Find index of d in z axis
+    idx = np.where(z >= d)[1][0]
+    
+    phi = model(z, mua_meas, mus_meas/0.2, fx)
+    phi_d = phi[:,:,idx]  # phi(d) [N x M x 1]
+    phi_sum = np.sum(phi**2 * dz, axis=-1)  # integral of phi**2 [N x M]
+    
+    a = alpha(phi, z, d)  # [N x M x Z]
+    # import pdb; pdb.set_trace()  # DEBUG
+    J = np.zeros((phi.shape[0], x0.size))  # initialize
+    J[:,0] = np.squeeze(1/phi_sum * phi_d**2)
+    J[:,1] = np.squeeze(a)
+    J[:,2] = np.squeeze(1-a)
+    
+    return J
+#%%
 # Scattering measurements
 data_path = getFile('Select data path')
 data = loadmat('{}'.format(data_path))
 
 #%%
+# Some lists of parameters
 keys = [x for x in data.keys() if 'ml' in x]
 keys.sort()  # just in case they are not in order
 
 F = np.arange(0,0.55,0.05)
 FX = np.array([np.mean(F[x:x+4]) for x in range(8)])  # spatial frequency (average)
-FX = FX[:,np.newaxis]  # reshape, dimensions: (N x 1)
 
 Z = np.arange(0,10,0.001)  # depth, micrometer resolution
 Z = Z [np.newaxis,:]  # reshape, dimensions: (1 x Z)
+
+WV = np.array([458, 520, 536, 556, 626])  # wavelengths
+
+phi_mod = mod_dP1  # model to calculate fluence
 
 # mus array: [D x FX x WV]
 mus_meas = np.zeros((len(keys), data[keys[0]].shape[0], data[keys[0]].shape[1]), dtype=float)
@@ -97,5 +133,34 @@ d_std = np.array([0.0035, 0.0136, 0.0216, 0.0401, 0.0655])
 mus_top = data['TiObaseTop'][0,:,1]
 mus_bot = data['AlObaseTop'][0,:,1]
 
-for _d, d in d_real:  # loop over thickness
-    
+opt_ret = [[]]  # save all the results of optimization, for debug. Should be 2D [D x WW] array
+# bound = Bounds(lb=[0,0,0], ub=[10,20,20])  # Add some physical limits to problem
+bound = ([0,0,0], [10,20,20])  # Add some physical limits to problem
+
+# results array, for easy copy-paste to excel
+ret_d = np.zeros([5,5])
+ret_must = np.zeros([5,5])
+ret_musb = np.zeros([5,5])
+
+for _d, d in enumerate(d_real):  # loop over thickness
+    if _d > 0:
+        opt_ret.append([])
+    for _w, w in enumerate(WV):  # loop over wavelengths
+        x0 = np.array([0.1, 1, 1])  # initial guess
+        temp = least_squares(target_fun, x0, jac=jacob, bounds=bound, method='trf',
+                             x_scale=np.array([0.1,1,1]), loss='linear', max_nfev=1e3, verbose=1,
+                             args=(mua_meas[_d,:,_w:_w+1], mus_meas[_d,:,_w:_w+1], Z, FX),
+                             kwargs={'model':phi_mod} )
+        opt_ret[_d].append(temp)
+        ret_d[_d,_w] = temp['x'][0]
+        ret_must[_d,_w] = temp['x'][1]
+        ret_musb[_d,_w] = temp['x'][2]
+
+#%% Plots
+from matplotlib import pyplot as plt
+W = 0  # wavelength index
+D = 0  # thickness index
+
+# f_ax = np.arange(0,0.5,0.01)
+mus_mod = model_fun((ret_d[D, W], ret_must[D, W], ret_musb[D, W]),
+                    mua_meas[D,:,:], mus_meas[D,:,:], Z, FX)
