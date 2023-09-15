7d9d7da6359f2e5027bf10dcd115cd899b35d5a5
Luigi Belcastro <luigi.belcastro@liu.se>
2023-09-15 17:58:38 +0200

    More statustics

--- analysis/pigStatistics.py
+++ analysis/pigStatistics.py
@@ -41,19 +41,141 @@ stem_w1 = data['PD4_w1']
 stem_w2 = data['PD4_w2']
 
 #%% some statistics
-control_w0_mean = np.nanmean(control_w0, axis=0)
-control_w1_mean = np.nanmean(control_w1, axis=0)
-control_w2_mean = np.nanmean(control_w2, axis=0)
+# First, re-assign variables for simplicity
+# Negative control
+mua_control_w0 = control_w0[:,1:3,2,0]
+mua_control_w0 = mua_control_w0[~np.isnan(mua_control_w0).any(axis=1)]  # remove nan
+mua_control_w1 = control_w1[:,1:3,2,0]
+mua_control_w1 = mua_control_w1[~np.isnan(mua_control_w1).any(axis=1)]  # remove nan
+mua_control_w2 = control_w2[:,1:3,2,0]
+mua_control_w2 = mua_control_w2[~np.isnan(mua_control_w2).any(axis=1)]  # remove nan
+# Stem cells
+mua_stem_w0 = stem_w0[:,1:3,2,0]
+mua_stem_w0 = mua_stem_w0[~np.isnan(mua_stem_w0).any(axis=1)]  # remove nan
+mua_stem_w1 = stem_w1[:,1:3,2,0]
+mua_stem_w1 = mua_stem_w1[~np.isnan(mua_stem_w1).any(axis=1)]  # remove nan
+mua_stem_w2 = stem_w2[:,1:3,2,0]
+mua_stem_w2 = mua_stem_w2[~np.isnan(mua_stem_w2).any(axis=1)]  # remove nan
 
-control_w0_std = np.nanstd(control_w0, axis=0)
-control_w1_std = np.nanstd(control_w1, axis=0)
-control_w2_std = np.nanstd(control_w2, axis=0)
+# Negative control
+mus_control_w0 = control_w0[:,1:3,:,1]
+mus_control_w0 = mus_control_w0[~np.isnan(mus_control_w0).any(axis=(1,2))]  # remove nan
+mus_control_w1 = control_w1[:,1:3,:,1]
+mus_control_w1 = mus_control_w1[~np.isnan(mus_control_w1).any(axis=(1,2))]  # remove nan
+mus_control_w2 = control_w2[:,1:3,:,1]
+mus_control_w2 = mus_control_w2[~np.isnan(mus_control_w2).any(axis=(1,2))]  # remove nan
+# Stem cells
+mus_stem_w0 = stem_w0[:,1:3,:,1]
+mus_stem_w0 = mus_stem_w0[~np.isnan(mus_stem_w0).any(axis=(1,2))]  # remove nan
+mus_stem_w1 = stem_w1[:,1:3,:,1]
+mus_stem_w1 = mus_stem_w1[~np.isnan(mus_stem_w1).any(axis=(1,2))]  # remove nan
+mus_stem_w2 = stem_w2[:,1:3,:,1]
+mus_stem_w2 = mus_stem_w2[~np.isnan(mus_stem_w2).any(axis=(1,2))]  # remove nan
 
-# Now fit scattering
-opt, cov = curve_fit(fit_fun, wv, control_w1_mean[2,:,1])
-perr = np.sqrt(np.diag(cov))  # standard deviation of fit
-#%% p-values, control vs stem cells
-# Absorption at 530 nm
-t0, p0 = ttest_ind(control_w0[:,:,2,0], stem_w0[:,:,2,0], axis=0, equal_var=False, nan_policy='omit')
-t1, p1 = ttest_ind(control_w1[:,:,2,0], stem_w1[:,:,2,0], axis=0, equal_var=False, nan_policy='omit')
-t2, p2 = ttest_ind(control_w2[:,:,2,0], stem_w2[:,:,2,0], axis=0, equal_var=False, nan_policy='omit')
+# Now fit scattering. Might have to loop
+# Negative control
+A_control_w0 = np.zeros((mus_control_w0.shape[:-1]))
+B_control_w0 = np.zeros((mus_control_w0.shape[:-1]))
+A_control_w1 = np.zeros((mus_control_w1.shape[:-1]))
+B_control_w1 = np.zeros((mus_control_w1.shape[:-1]))
+A_control_w2 = np.zeros((mus_control_w2.shape[:-1]))
+B_control_w2 = np.zeros((mus_control_w2.shape[:-1]))
+# Stem cells
+A_stem_w0 = np.zeros((mus_stem_w0.shape[:-1]))
+B_stem_w0 = np.zeros((mus_stem_w0.shape[:-1]))
+A_stem_w1 = np.zeros((mus_stem_w1.shape[:-1]))
+B_stem_w1 = np.zeros((mus_stem_w1.shape[:-1]))
+A_stem_w2 = np.zeros((mus_stem_w2.shape[:-1]))
+B_stem_w2 = np.zeros((mus_stem_w2.shape[:-1]))
+
+#%% This will take time, so run only once if possible
+# control w0
+for f in range(2):  # 2 frequencies
+    for i in range(len(mus_control_w0)):
+        temp, _ = curve_fit(fit_fun, wv, mus_control_w0[i,f,:],
+                            p0=[10, 1], method='trf', loss='soft_l1', max_nfev=2000)
+        A_control_w0[i, f] = temp[0]
+        B_control_w0[i, f] = temp[1]
+# control w1
+for f in range(2):  # 2 frequencies
+    for i in range(len(mus_control_w1)):
+        temp, _ = curve_fit(fit_fun, wv, mus_control_w1[i,f,:],
+                            p0=[10, 1], method='trf', loss='soft_l1', max_nfev=2000)
+        A_control_w1[i, f] = temp[0]
+        B_control_w1[i, f] = temp[1]
+# control w2
+for f in range(2):  # 2 frequencies
+    for i in range(len(mus_control_w2)):
+        temp, _ = curve_fit(fit_fun, wv, mus_control_w2[i,f,:],
+                            p0=[10, 1], method='trf', loss='soft_l1', max_nfev=2000)
+        A_control_w2[i, f] = temp[0]
+        B_control_w2[i, f] = temp[1]
+
+# stem cells w0
+for f in range(2):  # 2 frequencies
+    for i in range(len(mus_stem_w0)):
+        temp, _ = curve_fit(fit_fun, wv, mus_stem_w0[i,f,:],
+                            p0=[10, 1], method='trf', loss='soft_l1', max_nfev=2000)
+        A_stem_w0[i, f] = temp[0]
+        B_stem_w0[i, f] = temp[1]
+# stem cells w1
+for f in range(2):  # 2 frequencies
+    for i in range(len(mus_stem_w1)):
+        temp, _ = curve_fit(fit_fun, wv, mus_stem_w1[i,f,:],
+                            p0=[10, 1], method='trf', loss='soft_l1', max_nfev=2000)
+        A_stem_w1[i, f] = temp[0]
+        B_stem_w1[i, f] = temp[1]
+# stem cells w2
+for f in range(2):  # 2 frequencies
+    for i in range(len(mus_stem_w2)):
+        temp, _ = curve_fit(fit_fun, wv, mus_stem_w2[i,f,:],
+                            p0=[10, 1], method='trf', loss='soft_l1', max_nfev=2000)
+        A_stem_w2[i, f] = temp[0]
+        B_stem_w2[i, f] = temp[1]
+
+
+#%% Absorption at 530 nm
+# Control vs stem cells
+t0, p0 = ttest_ind(mua_control_w0, mua_stem_w0, axis=0, equal_var=False, nan_policy='omit')
+t1, p1 = ttest_ind(mua_control_w1, mua_stem_w1, axis=0, equal_var=False, nan_policy='omit')
+t2, p2 = ttest_ind(mua_control_w2, mua_stem_w2, axis=0, equal_var=False, nan_policy='omit')
+
+# Week0 vs week1
+tc, pc = ttest_ind(mua_control_w1, mua_control_w2, axis=0, equal_var=False, nan_policy='omit' )
+ts, ps = ttest_ind(mua_stem_w1, mua_stem_w2, axis=0, equal_var=False, nan_policy='omit' )
+
+# f1 vs f2
+t0, p0 = ttest_ind(mua_stem_w0[:,0], mua_stem_w0[:,1], axis=0, equal_var=False, nan_policy='omit')
+t1, p1 = ttest_ind(mua_stem_w1[:,0], mua_stem_w1[:,1], axis=0, equal_var=False, nan_policy='omit')
+t2, p2 = ttest_ind(mua_stem_w2[:,0], mua_stem_w2[:,1], axis=0, equal_var=False, nan_policy='omit')
+
+
+#%% log(A) parameter
+# Control vs stem cells
+t0, p0 = ttest_ind(np.log(A_control_w0), np.log(A_stem_w0), axis=0, equal_var=False, nan_policy='omit')
+t1, p1 = ttest_ind(np.log(A_control_w1), np.log(A_stem_w1), axis=0, equal_var=False, nan_policy='omit')
+t2, p2 = ttest_ind(np.log(A_control_w2), np.log(A_stem_w2), axis=0, equal_var=False, nan_policy='omit')
+
+# Week0 vs week1
+tc, pc = ttest_ind(np.log(A_control_w1), np.log(A_control_w2), axis=0, equal_var=False, nan_policy='omit' )
+ts, ps = ttest_ind(np.log(A_stem_w1), np.log(A_stem_w2), axis=0, equal_var=False, nan_policy='omit' )
+
+# f1 vs f2
+t0, p0 = ttest_ind(np.log(A_stem_w0[:,0]), np.log(A_stem_w0[:,1]), axis=0, equal_var=False, nan_policy='omit')
+t1, p1 = ttest_ind(np.log(A_stem_w1[:,0]), np.log(A_stem_w1[:,1]), axis=0, equal_var=False, nan_policy='omit')
+t2, p2 = ttest_ind(np.log(A_stem_w2[:,0]), np.log(A_stem_w2[:,1]), axis=0, equal_var=False, nan_policy='omit')
+
+#%% B parameter
+# Control vs stem cells
+t0, p0 = ttest_ind(B_control_w0, B_stem_w0, axis=0, equal_var=False, nan_policy='omit')
+t1, p1 = ttest_ind(B_control_w1, B_stem_w1, axis=0, equal_var=False, nan_policy='omit')
+t2, p2 = ttest_ind(B_control_w2, B_stem_w2, axis=0, equal_var=False, nan_policy='omit')
+
+# Week0 vs week1
+tc, pc = ttest_ind(B_control_w1, B_control_w2, axis=0, equal_var=False, nan_policy='omit' )
+ts, ps = ttest_ind(B_stem_w1, B_stem_w2, axis=0, equal_var=False, nan_policy='omit' )
+
+# f1 vs f2
+t0, p0 = ttest_ind(B_stem_w0[:,0], B_stem_w0[:,1], axis=0, equal_var=False, nan_policy='omit')
+t1, p1 = ttest_ind(B_stem_w1[:,0], B_stem_w1[:,1], axis=0, equal_var=False, nan_policy='omit')
+t2, p2 = ttest_ind(B_stem_w2[:,0], B_stem_w2[:,1], axis=0, equal_var=False, nan_policy='omit')
